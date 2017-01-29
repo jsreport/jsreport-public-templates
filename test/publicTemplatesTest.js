2b6fc1c12a462b27816be2c147c55ee5d9b1973d
honza.pofider@seznam.cz <honza.pofider@seznam.cz>
2017-01-29 11:40:11 +0100

    replace q with bluebird

--- test/publicTemplatesTest.js
+++ test/publicTemplatesTest.js
@@ -19,61 +19,57 @@ var authOptions = {
 describe('public-templates', function () {
   var reporter
 
-  beforeEach(function (done) {
+  beforeEach(function () {
     reporter = new Reporter({
       rootDirectory: path.join(__dirname, '../'),
       authentication: authOptions
     })
 
-    reporter.init().then(function () {
-      done()
-    }).fail(done)
+    return reporter.init()
   })
 
-  it('generating read sharing token should add readSharingToken into template', function (done) {
-    reporter.documentStore.collection('templates').insert({
+  it('generating read sharing token should add readSharingToken into template', function () {
+    return reporter.documentStore.collection('templates').insert({
       content: 'content', engine: 'none', recipe: 'html', name: 'foo'
     }).then(function (template) {
       return reporter.publicTemplates.generateSharingToken(template.shortid, 'read').then(function (token) {
         should(token).ok
       }).then(function () {
         return reporter.documentStore.collection('templates').find({shortid: template.shortid}).then(function (template) {
-          template[0].readSharingToken.should.be.ok
-          done()
+          template[0].readSharingToken.should.be.ok          
         })
       })
-    }).catch(done)
+    })
   })
 
-  it('generating write sharing token should add writeSharingToken into template', function (done) {
-    reporter.documentStore.collection('templates').insert({
+  it('generating write sharing token should add writeSharingToken into template', function () {
+    return reporter.documentStore.collection('templates').insert({
       content: 'content', engine: 'none', recipe: 'html', name: 'foo'
     }).then(function (template) {
       return reporter.publicTemplates.generateSharingToken(template.shortid, 'write').then(function (token) {
         should(token).ok
       }).then(function () {
         return reporter.documentStore.collection('templates').find({shortid: template.shortid}).then(function (template) {
-          template[0].writeSharingToken.should.be.ok
-          done()
+          template[0].writeSharingToken.should.be.ok       
         })
       })
-    }).catch(done)
+    })
   })
 
-  it('rendering report should fail when req.options.authorization.readToken not specified', function (done) {
-    reporter.documentStore.collection('templates').insert({
+  it('rendering report should fail when req.options.authorization.readToken not specified', function () {
+    return reporter.documentStore.collection('templates').insert({
       content: 'content', engine: 'none', recipe: 'html', name: 'foo'
     }).then(function (template) {
       return reporter.render({template: {shortid: template.shortid}}).then(function () {
-        done(new Error('Rendering report without auth options should fail.'))
+        throw new Error('Rendering report without auth options should fail.')
       }).catch(function (e) {
-        done()
+        e.message.should.not.be.eql('Rendering report without auth options should fail.')
       })
-    }).catch(done)
+    })
   })
 
-  it('rendering report should fail when req.options.authorization.readToken has invalid token', function (done) {
-    reporter.documentStore.collection('templates').insert({
+  it('rendering report should fail when req.options.authorization.readToken has invalid token', function () {
+    return reporter.documentStore.collection('templates').insert({
       content: 'content', engine: 'none', recipe: 'html', name: 'foo'
     }).then(function (template) {
       var req = {
@@ -85,15 +81,15 @@ describe('public-templates', function () {
         options: {authorization: {readToken: 'invalid'}}
       }
       return reporter.render(req).then(function () {
-        done(new Error('Rendering report without auth options should fail.'))
+        throw new Error('Rendering report without auth options should fail.')
       }).catch(function (e) {
-        done()
+        e.message.should.not.be.eql('Rendering report without auth options should fail.')
       })
-    }).catch(done)
+    })
   })
 
-  it('rendering report should succeed with valid req.options.authorization.readToken', function (done) {
-    reporter.documentStore.collection('templates').insert({
+  it('rendering report should succeed with valid req.options.authorization.readToken', function () {
+    return reporter.documentStore.collection('templates').insert({
       content: 'content', engine: 'none', recipe: 'html', name: 'foo', readSharingToken: 'token'
     }).then(function (template) {
       var req = {
@@ -104,14 +100,12 @@ describe('public-templates', function () {
         body: {options: {authorization: {readToken: 'token'}}},
         options: {authorization: {readToken: 'token'}}
       }
-      return reporter.render(req).then(function () {
-        done()
-      })
-    }).catch(done)
+      return reporter.render(req)
+    })
   })
 
-  it('rendering report should succeed with valid req.options.authorization.writeToken', function (done) {
-    reporter.documentStore.collection('templates').insert({
+  it('rendering report should succeed with valid req.options.authorization.writeToken', function () {
+    return reporter.documentStore.collection('templates').insert({
       content: 'content', engine: 'none', recipe: 'html', name: 'foo', writeSharingToken: 'token'
     }).then(function (template) {
       var req = {
@@ -122,15 +116,13 @@ describe('public-templates', function () {
         template: {shortid: template.shortid},
         query: {}
       }
-      return reporter.render(req).then(function () {
-        done()
-      })
-    }).catch(done)
+      return reporter.render(req)
+    })
   })
 
-  it('rendering report with req.options.authorization.grantRead should add token to the template', function (done) {
+  it('rendering report with req.options.authorization.grantRead should add token to the template', function () {
     var req = {user: {_id: 'foo'}}
-    reporter.documentStore.collection('templates').insert({
+    return reporter.documentStore.collection('templates').insert({
       content: 'content', engine: 'none', recipe: 'html', name: 'foo'
     }, req).then(function (template) {
       return reporter.render({
@@ -139,16 +131,15 @@ describe('public-templates', function () {
         user: {_id: 'foo'}
       }).then(function () {
         return reporter.documentStore.collection('templates').find({shortid: template.shortid}).then(function (templates) {
-          templates[0].readSharingToken.should.be.ok
-          done()
+          templates[0].readSharingToken.should.be.ok       
         })
       })
-    }).catch(done)
+    })
   })
 
-  it('rendering report with req.options.authorization.grantWrite should add token to the template', function (done) {
+  it('rendering report with req.options.authorization.grantWrite should add token to the template', function () {
     var req = {user: {_id: 'foo'}}
-    reporter.documentStore.collection('templates').insert({
+    return reporter.documentStore.collection('templates').insert({
       content: 'content', engine: 'none', recipe: 'html', name: 'foo'
     }, req).then(function (template) {
       return reporter.render({
@@ -157,11 +148,10 @@ describe('public-templates', function () {
         user: {_id: 'foo'}
       }).then(function () {
         return reporter.documentStore.collection('templates').find({shortid: template.shortid}).then(function (templates) {
-          templates[0].writeSharingToken.should.be.ok
-          done()
+          templates[0].writeSharingToken.should.be.ok        
         })
       })
-    }).catch(done)
+    })
   })
 })
 
