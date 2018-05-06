3166b279eedfda9960f205e411e701d2ff41f248
BJR Matos <bjrmatos@gmail.com>
2018-05-06 11:31:50 -0500

    update min node engine

--- jsreport.config.js
+++ jsreport.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   'name': 'public-templates',
   'main': 'lib/main.js',
-  'dependencies': [ 'templates', 'authentication', 'authorization' ],
+  'dependencies': ['templates', 'authentication', 'authorization'],
   'embeddedSupport': true,
   'skipInExeRender': true
 }
